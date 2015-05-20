2b1951c5bea055aa424bf7c1e8ee963b162fb012
cooljddj <cooljddj@gmail.com>
2015-05-20 10:54:10 +1000

    Update autograb.js

--- autograb.js
+++ autograb.js
@@ -5,16 +5,16 @@ API.on(API.DJ_ADVANCE, grab);
 function grab() {
         setTimeout(function(){$(".icon-curate").click()}, 60000);
         setTimeout(function(){$($(".curate").children(".menu").children().children()[0]).mousedown()}, 60000);
-        API.chatLog(API.getMedia().title + " will be grab in 60 seconds");
+        API.chatLog(API.getMedia().title + " will be grabed in 60 seconds");
 }
 
 function command(value) {
     if (value == "/start") {
         API.on(API.DJ_ADVANCE, grab);
-        API.chatLog("Auto-Grab was start", alert);
+        API.chatLog("Auto-Grab was started", alert);
     }
     else if (value == "/stop") {
         API.off(API.DJ_ADVANCE, grab);
-        API.chatLog("Auto-Grab was stop", alert);
+        API.chatLog("Auto-Grab was stopped", alert);
     }
 }
